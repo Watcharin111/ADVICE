c2b8c37a4a7ad939caafd79e39206397cb54cc23
Watcharin111 <55044251+Watcharin111@users.noreply.github.com>
2019-10-13 14:49:51 +0700

    Add project files.

---
+++ ADVICE/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace ADVICE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Orange;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(1, 364);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(839, 166);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(291, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(288, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ประยูทธ์ จันโอชา";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Maroon;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(1, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(292, 365);
+            this.panel2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(38, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 32);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "time";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(315, 35);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 528);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ ADVICE/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADVICE
+{
+    
+    public partial class Form1 : Form
+    {
+        
+        int x = 255, y = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            label1.SetBounds(x,y,1,1);
+            x--;
+            if (x <= -10)
+            {
+                x = 500;
+            }
+
+            int hh = DateTime.Now.Hour;
+            int mm = DateTime.Now.Minute;
+            int ss = DateTime.Now.Second;
+            
+            String time = "";
+            if (hh > 10 && mm > 10 && ss <10)
+            {
+                label2.Text = "" + hh + ":" + mm + ":0" + ss;
+            }
+            else if (hh > 10 && mm < 10 && ss < 10)
+            {
+                label2.Text = "" + hh + ":0" + mm + ":0" + ss;
+            }
+
+
+
+            else if (hh < 10 && mm <10 && ss < 10)
+            {
+                label2.Text = "0" + hh + ":0" + mm + ":0" + ss;
+            }
+            else
+            {
+                label2.Text = "" + hh + ":" + mm + ":" + ss;
+            }
+           // label2.Text = hh+":"+mm+":"+ss;
+
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+           
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+            label1.Text = "ประยุทธ์ จันโอชา";
+            label1.Font = new Font("", 25, FontStyle.Bold);
+            timer1.Interval = 1;
+            timer1.Start();
+
+        }
+    }
+}
